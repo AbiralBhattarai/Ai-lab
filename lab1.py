cd465ee35061c294de98f7b046a1a4525d6b2fe9
Abiral <abiralbh.101@gmail.com>
2023-11-28 12:35:23 +0545

    lab1

---
+++ lab1.py
@@ -0,0 +1,7 @@
+class Mybook:
+    def __init__(self,name,author,publish_date):
+        self.name = name
+        self.author = author
+        self.publish_date = publish_date
+
+book1 = Mybook('hero','abiral',2020)
